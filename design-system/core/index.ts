87136f15a2631ced486a3cfd7922e04fb6a785e2
nabn <nabeen.khadka@gmail.com>
2023-05-04 20:10:12 +1000

    chore: remove unused Slider

--- design-system/core/index.ts
+++ design-system/core/index.ts
@@ -4,7 +4,6 @@ export * from './GluestackUIProvider';
 export * from './Hstack';
 export * from './Image';
 export * from './Input';
-export * from './Slider';
 export * from './Text';
 export * from './Vstack';
 export * from './styled';
